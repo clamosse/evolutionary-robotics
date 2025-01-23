85ace05c06e4e4e35f532caf27fb56e817fb7bc7
calvi <clamosse@uvm.edu>
2025-01-22 20:10:18 -0500

    added one link

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import time as t
 physicsClient = p.connect(p.GUI)
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+p.loadSDF("box.sdf")
+
 for i in range(0,1000):
     p.stepSimulation()
     t.sleep(1/60)
