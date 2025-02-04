4465a3de6eabf2b2aa9565b15c112ff6b84b3941
calvi <clamosse@uvm.edu>
2025-02-04 12:37:26 -0500

    added legs and leg joints

--- generate.py
+++ generate.py
@@ -1,7 +1,22 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-2,2,.5], size=[1,1,1])
+    pyrosim.End()
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
 
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5], size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_Front_Leg" , parent= "Torso" , child = "Front Leg" , type = "revolute", position = [.5,0,1])
+    pyrosim.Send_Joint( name = "Torso_Back_Leg" , parent= "Torso" , child = "Back Leg" , type = "revolute", position = [-.5,0,1])
+    pyrosim.Send_Cube(name="Front Leg", pos=  [.5,0,-.5], size=[1,1,1])
+    pyrosim.Send_Cube(name="Back Leg", pos=  [-.5,0,-.5], size=[1,1,1])
+    pyrosim.End()
+
+Create_World()
+
+Create_Robot()
+
+
--- simulate.py
+++ simulate.py
@@ -1,10 +1,16 @@
+import pybullet_data
 import pybullet as p
 import time as t
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-p.loadSDF("box.sdf")
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 
 for i in range(0,1000):
     p.stepSimulation()
