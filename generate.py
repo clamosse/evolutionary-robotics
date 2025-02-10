737edc77b34bd75332a59c1c0353a1080c3b6030
calvi <clamosse@uvm.edu>
2025-02-10 13:55:43 -0500

    sensor sensing data and outputing to file

--- generate.py
+++ generate.py
@@ -9,10 +9,11 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5], size=[1,1,1])
-    pyrosim.Send_Joint( name = "Torso_Front_Leg" , parent= "Torso" , child = "Front Leg" , type = "revolute", position = [.5,0,1])
-    pyrosim.Send_Joint( name = "Torso_Back_Leg" , parent= "Torso" , child = "Back Leg" , type = "revolute", position = [-.5,0,1])
-    pyrosim.Send_Cube(name="Front Leg", pos=  [.5,0,-.5], size=[1,1,1])
-    pyrosim.Send_Cube(name="Back Leg", pos=  [-.5,0,-.5], size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [.5,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=  [.5,0,-.5], size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [-.5,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=  [-.5,0,-.5], size=[1,1,1])
+
     pyrosim.End()
 
 Create_World()
