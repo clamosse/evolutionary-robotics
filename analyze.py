426193378bf118718aff9456bfa83a049a7cdfe6
calvi <clamosse@uvm.edu>
2025-02-13 20:33:06 -0500

    robot walks

--- analyze.py
+++ analyze.py
@@ -4,9 +4,19 @@ import matplotlib.pyplot
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
 frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
 
+#matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg", linewidth = 3)
+#matplotlib.pyplot.plot(frontLegSensorValues, label = "Front Leg")
 
-matplotlib.pyplot.plot(backLegSensorValues, label = "Back Leg", linewidth = 3)
-matplotlib.pyplot.plot(frontLegSensorValues, label = "Front Leg")
+#matplotlib.pyplot.legend()
+#matplotlib.pyplot.show()
+
+
+frontLeg_targetAngles = np.load("data/frontLeg_targetAngles.npy")
+backLeg_targetAngles = np.load("data/backLeg_targetAngles.npy")
+
+matplotlib.pyplot.plot(frontLeg_targetAngles, label = "Front Leg Target Angles")
+matplotlib.pyplot.plot(backLeg_targetAngles, label = "Back Leg Target Angles")
 
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
+
--- simulate.py
+++ simulate.py
@@ -3,7 +3,7 @@ import pybullet_data
 import time as t
 import pyrosim.pyrosim as pyrosim
 import numpy as np
-import math
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -20,6 +20,22 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
+
+backLeg_amplitude = np.pi/4
+backLeg_frequency = 10
+backLeg_phaseOffset = 0
+
+backLeg_targetAngles = backLeg_amplitude * np.sin(backLeg_frequency * np.linspace(0, 2 * np.pi, 1000) + backLeg_phaseOffset)
+np.save('data/backLeg_targetAngles.npy', backLeg_targetAngles)
+
+
+frontLeg_amplitude = np.pi/4
+frontLeg_frequency = 10
+frontLeg_phaseOffset = np.pi/2
+
+frontLeg_targetAngles = frontLeg_amplitude * np.sin(frontLeg_frequency * np.linspace(0, 2 * np.pi, 1000) + frontLeg_phaseOffset)
+np.save('data/frontLeg_targetAngles.npy', frontLeg_targetAngles)
+
 for i in range(0,1000):
     p.stepSimulation()
 
@@ -30,15 +46,15 @@ for i in range(0,1000):
     bodyIndex = robotId,
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = -(math.pi)/6.0,
-    maxForce = 500)
+    targetPosition = backLeg_targetAngles[i],
+    maxForce = 50)
 
     pyrosim.Set_Motor_For_Joint(
     bodyIndex = robotId,
     jointName = "Torso_FrontLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = (math.pi)/6.0,
-    maxForce = 500)
+    targetPosition = frontLeg_targetAngles[i],
+    maxForce = 50)
 
     t.sleep(1/60)
 
