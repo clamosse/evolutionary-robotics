c177bba151961654e8cbff063a8837f93f763d46
calvi <clamosse@uvm.edu>
2025-03-16 12:32:52 -0400

    search generates and simulates 5 random fully connected robots

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -28,11 +29,9 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron(name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name = 4 , jointName = "Torso_FrontLeg")
 
-    pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = -10.0 )
-    pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 3 , weight = 10.0 )
-    pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 4 , weight = -1.0 )
-    pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.0 )
-
+    for sensorNeuronName in range(0,3):
+        for motorNeuronName in range(3,5):
+                pyrosim.Send_Synapse(sourceNeuronName = sensorNeuronName , targetNeuronName = motorNeuronName , weight = random.randrange(-1,1))
 
     pyrosim.End()
 
